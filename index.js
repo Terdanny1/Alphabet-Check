cac997010348807ea7fecd3849742e3e706e17f7
Terdanny1 <terdanconcept68@gmail.com>
2023-02-15 11:16:55 +0100

    added a reset function to the program

--- index.js
+++ index.js
@@ -7,18 +7,29 @@
 //      }
 // }
 
+//fuction to check if a letter is vowel or consonant
 function checkLetter() {
   const input = document.getElementById('input').value.toLowerCase()
   const vowels = ['a', 'e', 'i', 'o', 'u']
 
   if (vowels.includes(input)) {
-    document.getElementById('answer').innerHTML = `${input} is a vowel letter`
+    document.getElementById('result').innerHTML = `${input} is a vowel letter`
   }
-  else if (input.length === 1 && input.match()){
-
+  else if (input.length === 1 && input.match(/[a-z]/i)){
+    document.getElementById('result').innerHTML = `${input} is a consonant letter`
   }
   else{
-    document.getElementById('answer').innerHTML = "Please input a valid letter"
+    document.getElementById('result').innerHTML = "Please input a valid letter"
   }
 
+  //clear input field and reset text
+//   document.getElementById('input').value = ''
+//   setTimeout (() => XPathResult.innerHTML = '', 3000)
+
+}
+
+//funtion to reset it
+function reset(){
+    document.getElementById('input').value = ""
+    document.getElementById('result').innerHTML=""
 }
